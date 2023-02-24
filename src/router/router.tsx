64a427cc9ad1a425ce6bd9f8de2182515d89bbd4
tot_dlinnyy <gnngr333@gmail.com>
2023-02-24 20:18:30 +0700

    feat: add error page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,13 +5,14 @@ import {
 } from 'react-router-dom'
 
 import App from '../App'
+import Error from '../pages/error/Error'
 import NotFound from '../pages/notFound/NotFound'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<App />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path='/' element={<App />} errorElement={<Error />} />
+      <Route path='*' element={<NotFound />} errorElement={<Error />} />
     </>
   )
 )
