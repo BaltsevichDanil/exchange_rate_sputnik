bd136b60cd7e325683d15bff76714c3b1b7715d4
tot_dlinnyy <gnngr333@gmail.com>
2023-02-24 22:55:28 +0700

    feat: add currency page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,23 +1,23 @@
-import {Center, Heading, Link, VStack} from '@chakra-ui/react'
+import {Heading, Link} from '@chakra-ui/react'
 import {FC} from 'react'
 import {Link as RLink, useRouteError} from 'react-router-dom'
+
+import PageWrapper from '../../components/pageWrapper/PageWrapper'
 
 const Error: FC = () => {
   const error = useRouteError() as Error
   return (
-    <Center height='100vh'>
-      <VStack spacing='4'>
-        <Heading as='h1' size='4xl'>
-          Ошибка!
-        </Heading>
-        <Heading as='h5' color='red.500'>
-          {error.message}
-        </Heading>
-        <Link as={RLink} to='/'>
-          На главную
-        </Link>
-      </VStack>
-    </Center>
+    <PageWrapper>
+      <Heading as='h1' size='4xl'>
+        Ошибка!
+      </Heading>
+      <Heading as='h5' color='red.500'>
+        {error.message}
+      </Heading>
+      <Link as={RLink} to='/'>
+        На главную
+      </Link>
+    </PageWrapper>
   )
 }
 
