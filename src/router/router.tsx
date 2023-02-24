3a1424d5fd0f72b26469a60433f6c8248cc8921c
tot_dlinnyy <gnngr333@gmail.com>
2023-02-24 20:11:47 +0700

    feat: add page 404

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,9 +5,15 @@ import {
 } from 'react-router-dom'
 
 import App from '../App'
+import NotFound from '../pages/notFound/NotFound'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<App />} />)
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />} />
+      <Route path='*' element={<NotFound />} />
+    </>
+  )
 )
 
 export default router
